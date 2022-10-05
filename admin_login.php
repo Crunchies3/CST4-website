86f51d8655a3a1bd906a58e7917428448d13660f
kixks <114547887+kixks@users.noreply.github.com>
2022-10-05 23:37:22 +0800

    Error Fix
    Erorr Login Fix

--- admin_login.php
+++ admin_login.php
@@ -34,28 +34,31 @@ if (isset($_SESSION['staff_login'])) {
 
 <?php
 include 'config.php';
-if(isset($_POST['staff_login-btn'])){
-    
-    if(isset($_POST['admin_username'])){
+if (isset($_POST['staff_login-btn'])) {
+
+    if (isset($_POST['admin_username'])) {
         $admin_username = $_POST['admin_username'];
         $admin_password = $_POST['admin_password'];
     }
+
     //pag pangita if mag match ang admin input ug password sa database
-    $sql = "SELECT*FROM admin WHERE username = '$admin_username'";
+    $sql = "SELECT * FROM admin WHERE username = '$admin_username' and password = '$admin_password' ";
     $result = $conn->query($sql);
     //pagkuha sa mga elements sa database
-    $row = $result->fetch_assoc();
-    if($admin_username != $row['username'] && $admin_password != $row['password']){
-		//pag dili mag match kay mag error
-		echo '<script>alert("Incorrect Id/Password.")</script>';
-			
-		}
-    else{
-        //declare nato ni globally kay gamitunon ni admin_header.php nato
-        $_SESSION['admin_login'] = true;
-        $_SESSION['admin_name'] = $row['name'];
-        $_SESSION['admin_username'] = $row['username'];
-        header('location:admin_dashboard.php');
+    while ($row = $result->fetch_assoc()) {
+        $username = $row['username'];
+        $password = $row['password'];
+
+
+        if ($admin_username == $username && $admin_password == $password) {
+            $_SESSION['admin_login'] = true;
+            $_SESSION['admin_name'] = $row['name'];
+            $_SESSION['admin_username'] = $row['username'];
+            header('location:admin_dashboard.php');
+        }
     }
+    echo '<script>alert("iNCORRECT DETAILS")</script>';
 }
+
+
 ?>
