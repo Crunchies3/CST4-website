1c3f77fbfdc1626baf47632d44380adc9645b9b5
kixks <114547887+kixks@users.noreply.github.com>
2022-10-06 01:17:33 +0800

    Dtransferown Backend Full
    Full functionality of Dtransferown bank feature

--- DTransferown.php
+++ DTransferown.php
@@ -1,3 +1,65 @@
+<?php
+include 'config.php';
+session_start();
+$sql = "SELECT * FROM customers Where customer_id = $_SESSION[account_number]";
+$account_number = $_SESSION['account_number'];
+$result = mysqli_query($conn, $sql);
+$row = $result->fetch_assoc();
+
+if (isset($_POST['submit_transfer'])) {
+    if (isset($_POST['transfer_to'])) {
+        $transfer_to = $_POST['transfer_to'];
+        $amount_transfer = $_POST['amount_transfer'];
+    }
+    if ($account_number != $row['customer_id']) {
+        echo '<script>Account Number Didnt Match</script>';
+    } else {
+        $total_debit = $row['total_debit'] + $amount_transfer;
+        $net_balance = $row['net_balance'] - $amount_transfer;
+
+        $transaction_id = mt_rand(100, 999) . mt_rand(1000, 9999) . mt_rand(10, 99);
+
+        date_default_timezone_set('Asia/Kolkata');
+        $transaction_date = date("d/m/y h:i:s A");
+
+        $remark = "Cash Transfer";
+
+        $description = "Cash Transfer";
+
+        $conn->autocommit(FALSE);
+
+        $sql1 = "UPDATE customers SET net_balance = '$net_balance' WHERE customer_id = '$account_number' ";
+        $sql2 = "UPDATE customers SET total_debit = '$total_debit' WHERE customer_id = '$account_number' ";
+
+        $sql3 = "INSERT INTO passbook_$account_number(Transaction_id,Transaction_date,Description,Cr_amount,Dr_amount,Net_Balance,Remark)
+        VALUES('$transaction_id','$transaction_date','$description','0','$total_debit','$net_balance','$remark')";
+
+        $sql_other = "SELECT * FROM customers WHERE customer_id = '$transfer_to' ";
+        $result = mysqli_query($conn, $sql_other);
+        $row = $result->fetch_assoc();
+
+        $total_credit = $row['total_credit'] + $amount_transfer;
+        $other_net_balance = $row['net_balance'] + $amount_transfer;
+
+        $sql4 = "UPDATE customers SET net_balance = '$other_net_balance' WHERE customer_id = '$transfer_to' ";
+        $sql5 = "UPDATE customers SET total_credit = '$total_credit' WHERE customer_id = '$transfer_to' ";
+
+        $sql6 = "INSERT INTO passbook_$transfer_to(Transaction_id,Transaction_date,Description,Cr_amount,Dr_amount,Net_Balance,Remark)
+        VALUES('$transaction_id','$transaction_date','$description','$total_credit','0','$other_net_balance','$remark')";
+
+        if ($conn->query($sql1) == TRUE && $conn->query($sql2) == TRUE && $conn->query($sql3) == TRUE  && $conn->query($sql4) == TRUE  && $conn->query($sql5) == TRUE  && $conn->query($sql6) == TRUE) {
+            $conn->commit();
+            echo '<script>alert("Account Cash Transfer Successfull")
+                 location="DTransferown.php"</script>';
+        } else {
+            echo '<script>alert("Transaction failed\n\nReason:\n\n' . $conn->error . '")</script>';
+            $conn->rollback();
+        }
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -26,7 +88,7 @@
                         <div class="sidebaArea">
                             <div class="sidebarTopArea text-center">
                                 <div class="userName">
-                                    <h3>Cyril Charles O Alvez</h3>
+                                    <h3><?php echo $row['name']; ?></h3>
                                 </div>
                             </div>
                             <div class="slidebarNavArea">
@@ -38,12 +100,7 @@
                                         <li class="navItem submenu show active">
                                             <a href="DTransferown.php">Transfer
                                                 <span class="pull-right-container">
-                                                    <svg xmlns="http://www.w3.org/2000/svg"
-                                                        xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true"
-                                                        focusable="false" width="1em" height="1em"
-                                                        preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20"
-                                                        class="iconify" data-icon="dashicons:arrow-right-alt2"
-                                                        data-inline="false" style="transform: rotate(360deg);">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" aria-hidden="true" focusable="false" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 20 20" class="iconify" data-icon="dashicons:arrow-right-alt2" data-inline="false" style="transform: rotate(360deg);">
                                                         <path fill="currentColor" d="m6 15l5-5l-5-5l1-2l7 7l-7 7z">
                                                         </path>
                                                     </svg>
@@ -70,20 +127,20 @@
                                         <h4>OWN BANK TRANSFER</h4>
                                     </div>
                                 </div>
-                                
+
                             </div>
                             <div class="container">
-                                <form action="">
+                                <form method="post">
                                     <div class="mb-3" style="margin-top: 20px;">
                                         <label>Account Number</label>
-                                        <input type="text" style="margin-top: 10px;" class="form-control">
+                                        <input type="text" style="margin-top: 10px;" class="form-control" name="transfer_to">
                                     </div>
                                     <div class="mb-3" style="margin-top: 20px;">
                                         <label>Transfer Amount</label>
-                                        <input type="text" style="margin-top: 10px;" class="form-control">
+                                        <input type="text" style="margin-top: 10px;" class="form-control" name="amount_transfer">
                                     </div>
                                     <div style="margin-top: 20px;">
-                                        <button type="submit" class="btn buttColor">Submit</button>
+                                        <button type="submit" class="btn buttColor" name="submit_transfer">Submit</button>
                                     </div>
                                 </form>
                             </div>
